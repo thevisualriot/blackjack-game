290882c90cd963db53aea9c7b2d4068449bc2759
thevisualriot <rydzkowska@gmail.com>
2023-10-27 00:52:19 +0100

    checkResult function added

--- script.js
+++ script.js
@@ -36,45 +36,7 @@ while (keepPlaying) {
     dealerTotal += dealerRandom;
     playerTotal += playerRandom;
 
-
-    if (playerTotal < 21 && dealerTotal < 17) {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        dealerMove();
-        playerMove();
-
-    // player = 21 --- PLAYER WINS
-    } else if (playerTotal == 21) {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        alert(`You hit 21! You won!`);
-        location.reload();
-
-    // dealer > 17 --- stop
-    } else if (playerTotal < 21 && dealerTotal > 17 && dealerTotal < 21 ) {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        console.log(`The dealer stands`);
-        playerMove(playerRandom);
-
-    // dealer == 21 --- COMPUTER WINS
-    } else if (dealerTotal === 21){
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        alert(`The dealer hits 21 and wins!`);
-        location.reload();
-
-    } else if (dealerTotal > 21) {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        alert(`Lucky you! The dealer busts! You win!`);
-        location.reload();
-
-    } else if (playerTotal > 21 && dealerTotal > 21) {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        alert(`You both bust. Nobody wins.`);
-        location.reload();
-    }     
-    else {
-        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
-        alert(`You bust and your total is ${playerTotal}. You lost the game, loser!`);
-        location.reload();
-    }
+    checkResult();
 
     keepPlaying = stayOrNot(playerRandom, playerTotal);
 
@@ -109,14 +71,6 @@ while(!keepPlaying) {
 
 // FUNCTIONS
 
-function dealerMove(){
-    console.log(`Dealer total: ${dealerTotal}`);
-}
-
-function playerMove(){
-    console.log(`Player total: ${playerTotal}`);
-}
-
 function randomizeUpToEleven() {
     return Math.floor(Math.random() * 11) + 2;
 }
@@ -127,6 +81,36 @@ function stayOrNot(number, total) {
     return confirmation === true;
 }
 
+
+function checkResult() {
+
+    if (playerTotal === 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        alert(`You hit 21! You won!`);
+        location.reload();
+    } else if (dealerTotal === 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        alert(`The dealer hits 21 and wins!`);
+        location.reload();
+    } else if (dealerTotal > 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        alert(`Lucky you! The dealer busts! You win!`);
+        location.reload();
+    } else if (playerTotal > 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        alert(`You bust and your total is ${playerTotal}. You lost the game, loser!`);
+        location.reload();
+    } else if (playerTotal > 21 && dealerTotal > 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        alert(`You both bust. Nobody wins.`);
+        location.reload();
+    } else if (playerTotal < 21 && dealerTotal > 17 && dealerTotal < 21) {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+        console.log(`The dealer stands`);
+    } else {
+        console.log(`Player total: ${playerTotal} | Dealer total: ${dealerTotal}`);
+    }
+}
 
 
 
